11b666a23f093a285b45fda34162b39607f0a2f0
Ryan Ottney <rottney123@gmail.com>
2024-10-01 13:46:59 -0400

    importing TripGoRouting from src

--- src/TripGoRouting.js
+++ src/TripGoRouting.js
@@ -1,54 +1,24 @@
-(function (factory, window) {
+//import L from "/Users/ryan/Library/Caches/typescript/5.5/node_modules/@types/leaflet/index.d.ts";
 
-    // define an AMD module that relies on 'leaflet'
-    if (typeof define === 'function' && define.amd) {
-        define(['leaflet'], factory);
+export const tripgoRouting = {
+    has: function (obj, key) {
+        return obj != null && hasOwnProperty.call(obj, key);
+    },
 
-        // define a Common JS module that relies on 'leaflet'
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('leaflet'));
+    isFunction: function (obj) {
+        return typeof obj == 'function' || false;
+    },
+
+    contains: function (array, item) {
+        return array.indexOf(item) > -1;
+    },
+
+    validLatLng: function (latLng) {
+        return latLng !== undefined && latLng.lat !== undefined && latLng.lng !== undefined;
     }
+};
 
-    // attach your plugin to the global 'L' variable
-    if (typeof window !== 'undefined' && window.L) {
-        window.L.tripgoRouting = factory(L);
-    }
-}(function (L) {
-    var tripgoRouting =  {
-
-            // receives: json object and key
-            // returns: true if json object has key
-            has: function (obj, key) {
-                return obj != null && hasOwnProperty.call(obj, key);
-            },
-
-            // is a given object a function
-            isFunction: function (obj) {
-                return typeof obj == 'function' || false;
-            },
-
-            //returns true if the item is presents in array.
-            contains: function (array, item) {
-                return array.indexOf(item) > -1;
-            },
-
-            // returns true if latLng has lat and lng defined
-            validLatLng: function (latLng) {
-                return latLng !== undefined && latLng.lat !== undefined && latLng.lng !== undefined;
-            }
-
-        };
-
-
-    // return your plugin when you are done
-    return tripgoRouting;
-}, window));
-
-
-
-
-
-
-
-
-
+// If you want to automatically attach the plugin to Leaflet's global `L` object
+if (typeof window !== 'undefined' && window.L) {
+    window.L.tripgoRouting = tripgoRouting;
+}
