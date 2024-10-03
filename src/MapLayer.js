1081caeb322c43731b49ab4f1c0fa196c330da61
Ryan Ottney <rottney123@gmail.com>
2024-10-02 20:16:27 -0400

    removed unnecessary code

--- src/MapLayer.js
+++ src/MapLayer.js
@@ -9,13 +9,7 @@ export const mapLayer = (function() {
     let messenger = null;
     let tripgoApiKey = null;
     let floatPanel = false;
-    
 
-    function setGoogleTile() {
-        L.tileLayer('https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4i256!2m3!1e0!2sm!3i349018013!3m9!2sen-US!3sUS!5e18!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e0',{
-            attribution: '&copy; Powered by Google'
-        }).addTo(map);
-    };
 
     function setOSMTile() {
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -23,55 +17,14 @@ export const mapLayer = (function() {
         }).addTo(map);
     };
 
-    let defaultApiKey = "d72e9c4da23eec14ea56f4065abd95c8";
-    let defaultOptions = {
-        "mapId": "map",
-        "gogoleTile": true,
-        "mapCenter": {
-            "lat": -33.8650319,
-            "lng": 151.2039176
-        },
-        "floatPanel": false,
-        "tripgoApiKey": defaultApiKey
-    };
-
-    function checkOptions(options) {
-        if(options === undefined )
-            options = defaultOptions;
-        else{
-            if(! L.tripgoRouting.has(options,"mapId") || (options.mapId === undefined))
-                options.mapId = defaultOptions.mapId;
-            if(! L.tripgoRouting.has(options, "googleTile") || (options.googleTile === undefined))
-                options.gogoleTile = defaultOptions.googleTile;
-            if(! L.tripgoRouting.has(options, "mapCenter") || (options.mapCenter === undefined))
-                options.mapCenter = defaultOptions.mapCenter;
-            else{
-                if(!L.tripgoRouting.validLatLng(options.mapCenter)){
-                    console.error("Malformed map center coord");
-                    return false;
-                }
-            }
-             if(! L.tripgoRouting.has(options, "tripgoApiKey") || (options.tripgoApiKey.includes("TripGo")))
-                 options.tripgoApiKey =  defaultOptions.tripgoApiKey;
-
-            if(!L.tripgoRouting.has(options, "floatPanel"))
-                options.floatPanel = defaultOptions.floatPanel;
-
-        }
-        return options;
-    }
-
 
     return {
         initialize : function(options){
-            options = checkOptions(options);
-
             mapId = options.mapId;
             map = L.map(mapId).setView([options.mapCenter.lat,options.mapCenter.lng], 11);
-            (options.googleTile === undefined || options.googleTile) ? setGoogleTile() : setOSMTile();
+            setOSMTile();
 
             this.mapResize(window.innerWidth, window.innerHeight);
-            map.on('click', this.fromClick);
 
             tripgoApiKey = options.tripgoApiKey;
             floatPanel = options.floatPanel;
