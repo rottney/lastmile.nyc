dbf44881928ea669429bcf7ce756033c5e03f055
Ryan Ottney <rottney123@gmail.com>
2024-10-03 15:27:04 -0400

    added another attribution

--- src/MapLayer.js
+++ src/MapLayer.js
@@ -13,7 +13,8 @@ export const mapLayer = (function() {
 
     function setOSMTile() {
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            attribution: 'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | ' + 
+                '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
     };
 
